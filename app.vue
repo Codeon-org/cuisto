3bd48e85533f8104de430e4ba79e500be2c4cadb
MrAnyx <gentle.job6107@fastmail.com>
2025-02-03 21:45:56 +0000

    feat: add initial Nuxt project structure with basic components and configuration

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+    </div>
 </template>
+
+<style lang="scss">
+body {
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+    modules: [
+        "@nuxt/eslint",
+        "@nuxtjs/device",
+        "@vueuse/nuxt",
+        "@nuxt/image",
+        "@prisma/nuxt",
+        "@pinia/nuxt",
+        "pinia-plugin-persistedstate/nuxt",
+    ],
+    ssr: true,
+    devtools: { enabled: true },
+    router: {
+        options: {
+            scrollBehaviorType: "smooth"
+        }
+    },
+    compatibilityDate: "2024-11-01",
+    eslint: {
+        config: {
+            stylistic: {
+                indent: 4,
+                semi: true,
+                quotes: "double",
+                commaDangle: "only-multiline",
+                braceStyle: "allman",
+                quoteProps: "as-needed",
+            }
+        },
+    },
+    pinia: {
+        storesDirs: ["./stores/**"],
+    },
+    piniaPluginPersistedstate: {
+        storage: "localStorage",
+        key: "cuisto_%id"
+    },
+});
