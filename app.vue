a68385169ce7181dd6c3e3394905287f2ad77433
MrAnyx <gentle.job6107@fastmail.com>
2025-02-05 22:10:19 +0000

    feat: add PWA support with VitePWA integration and configure app manifest

--- app.vue
+++ app.vue
@@ -1,17 +1,9 @@
 <template>
     <div>
+        <VitePwaManifest/>
         <NuxtPage />
     </div>
 </template>
 
 <script setup lang="ts">
 </script>
-
-<style lang="scss">
-body {
-    font-family: 'Roboto', sans-serif;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-</style>
