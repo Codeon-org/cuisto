a1a9ae70ef7cb88fec002aaa4d94f03a6d2805f8
MrAnyx <gentle.job6107@fastmail.com>
2025-02-26 21:15:37 +0000

    feat: refactor pagination request validation; rename schema variable and utilize getValidatedQuery for improved query handling

--- server/utils/request.ts
+++ server/utils/request.ts
@@ -4,14 +4,14 @@ import { defu } from "defu";
 
 export const getPagination = (request: H3Event) =>
 {
-    const _schema = z.object({
+    const schema = z.object({
         page: validation.pagination.page.optional(),
         itemsPerPage: validation.pagination.itemsPerPage.optional(),
         sort: validation.pagination.sort.optional(),
         order: validation.pagination.order.optional(),
     });
 
-    const query = getQuery<z.infer<typeof _schema>>(request);
+    const query = getValidatedQuery(request, schema.parse);
 
     const pagination = defu(query, { page: 1, itemsPerPage: 10, sort: "createdAt", order: "desc" });
 
