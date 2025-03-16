e628b5769b4bf926dec2f03a265e53222c95c0dd
MrAnyx <gentle.job6107@fastmail.com>
2025-03-16 22:56:05 +0000

    feat: integrate barcode scanning functionality in index page; add StreamBarcodeReader component and update dependencies

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,14 +3,36 @@
         <ULink to="/test">
             Test
         </ULink>
+        <p>{{ code }}</p>
+        <pre>{{ err }}</pre>
+        <br>
+        <StreamBarcodeReader
+            @decode="onDecode"
+            @error="onError"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
-useSocket({
-    newUser: (name) =>
-    {
-        console.log(name);
-    }
-});
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+const code = ref("");
+const err = ref("");
+
+const onDecode = (res: string) =>
+{
+    code.value = res;
+};
+
+const onError = (res: string) =>
+{
+    err.value = res;
+};
+
+// useSocket({
+//     newUser: (name) =>
+//     {
+//         console.log(name);
+//     }
+// });
 </script>
--- server/api/ping.get.ts
+++ server/api/ping.get.ts
@@ -1,6 +1,4 @@
 import { DateTime } from "luxon";
-import Quagga from "@ericblade/quagga2"; // ES6
-import { decodeFromImage } from "@zxing/library";
 
 export default defineEventHandler(async () =>
 {
@@ -10,36 +8,15 @@ export default defineEventHandler(async () =>
     // const res = await $fetch("https://world.openfoodfacts.org/api/v2/product/3038354199603.json");
     // console.log(res);
 
-    // Quagga.decodeSingle(
-    //     {
-    //         src: "/app/.cache/image.jpeg",
-    //         numOfWorkers: 0, // Needs to be 0 when used within node
-    //         decoder: {
-    //             readers: ["code_128_reader"] // List of active readers
-    //         },
-    //     },
-    //     (result) =>
-    //     {
-    //         if (result?.codeResult)
-    //         {
-    //             console.log(result.codeResult.code);
-    //         }
-    //         else
-    //         {
-    //             console.log("No barcode detected");
-    //         }
-    //     }
-    // );
-
-    try
-    {
-        const result = await decodeFromImage(undefined, "/app/.cache/image.jpeg");
-        return { barcode: result.text };
-    }
-    catch
-    {
-        return { error: "Barcode not found" };
-    }
+    // try
+    // {
+    //     const result = await decodeFromImage(undefined, "/app/.cache/image.jpeg");
+    //     return { barcode: result.text };
+    // }
+    // catch
+    // {
+    //     return { error: "Barcode not found" };
+    // }
 
     return {
         status: "OK",
