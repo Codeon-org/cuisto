2f125e9708e54200c11805a82ee0b2e5003b6e4c
MrAnyx <gentle.job6107@fastmail.com>
2025-03-29 17:08:12 +0000

    feat: implement house update API endpoint with validation and ownership check

---
+++ server/api/houses/[houseId]/index.patch.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+const urlSchema = z.object({
+    houseId: validation.common.id
+});
+
+const bodySchema = z.object({
+    name: validation.house.name.optional(),
+});
+
+export default defineEventHandler(async (event) =>
+{
+    // Get houseId from url
+    const url = await getValidatedRouterParams(event, urlSchema.parse);
+
+    // Get the userId from the event context
+    const { id: userId } = event.context.user;
+
+    // Get the body from the event
+    const body = await readValidatedBody(event, bodySchema.parse);
+
+    // Check if the house exists and if the user is the owner
+    const house = await prisma.house.findFirst({
+        where: {
+            id: url.houseId,
+        }
+    });
+
+    // If the house doesn't exist, throw a 404 error
+    if (!house)
+    {
+        throw createError({
+            statusCode: 404,
+            statusMessage: "House not found",
+        });
+    }
+
+    if (house.ownerId !== userId)
+    {
+        throw createError({
+            statusCode: 403,
+            statusMessage: "You are not the owner of this house",
+        });
+    }
+
+    // Update the house
+    const updatedHouse = await prisma.house.update({
+        where: {
+            id: url.houseId,
+        },
+        data: body,
+    });
+
+    return updatedHouse;
+});
